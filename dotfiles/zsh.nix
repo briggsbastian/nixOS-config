9032d21fa22c62c123f1d380340341ae4507905b
briggsbastian <briggsbastian@pm.me>
2026-05-03 19:54:04 -0700

    somehow zsh.nix was empty so now its not

---
+++ dotfiles/zsh.nix
@@ -0,0 +1,24 @@
+{ ... }:
+{
+  programs.zsh {
+    enable = true;
+    enableAutoCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting. enable = true;
+
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch";
+      rebuild-test = "sudo nixos-rebuild test";
+
+      ls = "lsd";
+    };
+
+    history = {
+      size = 10000;
+      save = 10000;
+      ignoreDups = true;
+      share = true;
+    };
+  };
+}
+
