679093eb559ba53a977270afede8a650b075339a
briggsbastian <briggsbastian@pm.me>
2026-05-04 08:25:29 -0700

    moving alacritty to its own file pt.2

---
+++ dotfiles/alacritty.nix
@@ -0,0 +1,17 @@
+{ ... }: 
+{
+
+  programs.alacritty = { 
+    enable = true;
+    settings {
+      font = {
+        normal = { family = "JetBrainsMono Nerd Font"; style = "Regular"; };
+	bold = { family = "JetBrainsMono Nerd Font"; style = "Bold"; };
+	italic = { family = "JetBrainsMono Nerd Font"; style = "Italic"; };
+	size = 12;
+      };
+      window.opacity = 0.8;
+    };
+  };
+
+}
