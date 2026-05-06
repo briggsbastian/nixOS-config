ac2b5bb615374a78bd343c93a4123dc3b1c83785
briggsbastian <briggsbastian@pm.me>
2026-05-05 23:04:16 -0700

    base hyprland file and binds file

--- home-hyprland.nix
+++ home-hyprland.nix
@@ -5,9 +5,8 @@
     ./dotfiles/starship.nix
     ./dotfiles/jellyfin.nix
     ./dotfiles/zsh.nix
-    ./dotfiles/neovim.nix
-    ./dotfiles/alacritty.nix
-    ./dotfiles/tmux.nix
+    ./hyprland/hypr/hyprland.nix
+    ./hyprland/hypr/hypr-binds.nix
     ./hyprland/waybar/waybar.nix
   ];
   home.stateVersion = "25.11";
